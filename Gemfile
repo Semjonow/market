46d7f747fa9ba84810447843b652194eef917e5e
Juri Semjonov <juri.semjonov@gmail.com>
2013-05-25 12:20:56 +0300

    initial commit

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+
+group :assets do
+  gem 'sass-rails'
+  gem 'coffee-rails'
+  gem 'uglifier'
+  gem 'yui-compressor'
+end
+
+group :development do
+  gem 'therubyracer', group: :assets
+  gem 'html2haml'
+  gem 'capistrano'
+  gem 'thin'
+end
+
+gem 'pg'
+gem 'foreigner'
+gem 'activerecord-postgres-hstore'
+
+gem 'haml-rails'
+gem 'jquery-rails'
+
+gem 'unicorn'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Market::Application.config.secret_token = '3d71d4dfe473b5d0e97de2f15c9a12a64721a0859c4cf9593a65ef6ee4005b1394f485f4abdb0cccf78701974fe35a0cf60ee021304e640760049e485c685b2d'
